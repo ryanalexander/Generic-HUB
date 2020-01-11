ecc85b7255a45de2d05ed9e7d3cdcbd06eeb80e0
Tyler <tburnett96@outlook.com>
2020-01-11 09:35:41 -0600

    https://git.blockcade.net/Blockcade/HUB/issues/11
    Added MorphMenu
    Added Morph Buttons and Items.

--- src/main/java/net/blockcade/HUB/Common/Static/Inventory/menus/Cosmetic/MorphMenu.java
+++ src/main/java/net/blockcade/HUB/Common/Static/Inventory/menus/Cosmetic/MorphMenu.java
@@ -45,7 +45,25 @@ public class MorphMenu {
             Item zombie_head = new Item(Item.itemWithBase64(new ItemStack(Material.PLAYER_HEAD),"eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMTQzNjc5Njg4ZDYyNjZhOTY3OWIzOGU1YjVlM2M2MGFhYzY0NGY2MWVlZjI3NTBiMDYwNTQxODM0MjY4MmM5In19fQ=="),"&1Zombie");
             zombie_head.setLore("&7With this Morph you will be shown as","a &1Zombie&7 to all players on the Network","","&aClick to Transform");
 
+            Item skeleton_head = new Item(Item.itemWithBase64(new ItemStack(Material.PLAYER_HEAD),"eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYzc5ZTcyMzQ5MWM4ODZkY2MyNWU1ZDJkZjBlMDU3ODFmN2FjOWMzNDYyMTgzMmJkNzQ5NjVlZjc5YWNmOGYifX19=="),"&fSkeleton");
+            skeleton_head.setLore("&7With this Morph you will be shown as","a &fSkeleton&7 to all players on the Network","","&aClick to Transform");
+
+            Item enderman_head = new Item(Item.itemWithBase64(new ItemStack(Material.PLAYER_HEAD),"eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZDc0NmMyNmFhYzE2ZTM4ODA5ODJkMTM5OWZiNWY5Zjc5OTEzZGQ2MTZlNzE2NTRmOWJmNGQ0MWZiMzMifX19=="),"&5Enderman");
+            enderman_head.setLore("&7With this Morph you will be shown as","a &5Enderman&7 to all players on the Network","","&aClick to Transform");
+
+            Item cat_head = new Item(Item.itemWithBase64(new ItemStack(Material.PLAYER_HEAD),"eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZDc0NmMyNmFhYzE2ZTM4ODA5ODJkMTM5OWZiNWY5Zjc5OTEzZGQ2MTZlNzE2NTRmOWJmNGQ0MWZiMzMifX19=="),"&dCat");
+            cat_head.setLore("&7With this Morph you will be shown as","a &dCat&7 to all players on the Network","","&aClick to Transform");
+
+            Item creeper_head = new Item(Item.itemWithBase64(new ItemStack(Material.PLAYER_HEAD),"eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNjhlMjUyNmZiNGJkM2I3MDgxZWVkYmRlOTU4MGY0ZDZjNDdkNDBiZWRiNjM2ZjEwMjc4YWNlOWRmM2E1MmY1In19fQ=="),"&aCreeper");
+            creeper_head.setLore("&7With this Morph you will be shown as","a &aCreeper&7 to all players on the Network","","&aClick to Transform");
+
+
+
             inventory.setItem(29, zombie_head.spigot());
+            inventory.setItem(30, creeper_head.spigot());
+            inventory.setItem(31, enderman_head.spigot());
+            inventory.setItem(32, cat_head.spigot());
+            inventory.setItem(33, skeleton_head.spigot());
 
             return inventory;
     }
