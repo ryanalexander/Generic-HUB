392c6ff226eed185791a14be2b103cdbdbbc316f
Tyler <tburnett96@outlook.com>
2020-01-11 09:11:17 -0600

    Added MorphMenu

--- src/main/java/net/blockcade/HUB/Common/Static/Inventory/menus/Cosmetic/MorphMenu.java
+++ src/main/java/net/blockcade/HUB/Common/Static/Inventory/menus/Cosmetic/MorphMenu.java
@@ -1,12 +1,14 @@
-package net.blockcade.HUB.Common.Static.Inventory.menus.Preferences;
+package net.blockcade.HUB.Common.Static.Inventory.menus.Cosmetic;
 
 import net.blockcade.HUB.Common.GamePlayer;
 import net.blockcade.HUB.Common.Static.Inventory.Manager;
+import net.blockcade.HUB.Common.Static.Inventory.menus.CosmeticsMenu;
 import net.blockcade.HUB.Common.Static.Inventory.menus.Preferences.Filters.LightFilter;
 import net.blockcade.HUB.Common.Static.Inventory.menus.Preferences.Filters.MediumFilter;
 import net.blockcade.HUB.Common.Static.Inventory.menus.Preferences.Filters.NoFilter;
 import net.blockcade.HUB.Common.Static.Inventory.menus.Preferences.Filters.StrongFilter;
 import net.blockcade.HUB.Common.Static.Inventory.menus.PreferencesMenu;
+import net.blockcade.HUB.Common.Static.Variables.Cosmetics;
 import net.blockcade.HUB.Common.Utils.Item;
 import net.blockcade.HUB.Common.Utils.Text;
 import org.bukkit.Bukkit;
@@ -15,27 +17,28 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
-public class Censorship {
+public class MorphMenu {
+    public static Item getMenuItem() {
+        Item item = new Item(Material.CREEPER_HEAD,"&aMorph Menu");
 
-        public static Item getMenuItem() {
-            Item item = new Item(Material.HOPPER,"&fCensorship");
+        item.setLore("","&7Click to view all the possible morphs","");
+        item.setOnClick(p -> p.openInventory(getMenu(p)));
+        return item;
+    }
 
-            item.setLore("","&7View all Censorship related Preferences","");
-            item.setOnClick(p -> p.openInventory(getMenu(p)));
 
-            return item;
-        }
 
-        public static Inventory getMenu(Player player){
-            Inventory inventory = Bukkit.createInventory(null,54, Text.format("&7Particles"));
+    private static Inventory getMenu(Player player) {
+
+            Inventory inventory = Bukkit.createInventory(null,54, Text.format("&7Morph Menu"));
             GamePlayer gamePlayer = GamePlayer.getGamePlayer(player);
 
             Manager.addProfileHeader(inventory, gamePlayer);
 
             Item back = new Item(Item.itemWithBase64(new ItemStack(Material.PLAYER_HEAD),"eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvODY1MmUyYjkzNmNhODAyNmJkMjg2NTFkN2M5ZjI4MTlkMmU5MjM2OTc3MzRkMThkZmRiMTM1NTBmOGZkYWQ1ZiJ9fX0="),"&eBack");
-            back.setLore("&7Return to Preferences Menu");
+            back.setLore("&7Return to Cosmetics Menu");
             back.setOnClick((p)->{
-                p.openInventory(PreferencesMenu.getMenu(p));
+                p.openInventory(CosmeticsMenu.getMenu(p));
             });
             inventory.setItem(0,back.spigot());
 
@@ -48,7 +51,5 @@ public class Censorship {
 
 
             return inventory;
-        }
-
-
+    }
 }
